a81600983dea6127da7555b39fc02432eec36c5d
ozmarshall <parc.tec20@yahoo.com>
2022-03-07 18:52:04 -0500

    respuestas

---
+++ dia-5/01-librerias.py
@@ -0,0 +1,8 @@
+from camelcase import CamelCase
+
+instanciaCC = CamelCase('mundo')
+
+texto = 'bienvenidos al mundo de back'
+
+print(instanciaCC.hump(texto))
+
---
+++ dia-5/inicios-flask.py
@@ -0,0 +1,24 @@
+from doctest import debug
+from logging import info
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/api/info')
+def info_app():
+    return {
+        'fecha': datetime.now()
+    }
+
+@app.route('/')
+def inicial():
+   # pass
+   print('me llamaron')
+
+   return 'bienvenidos a mi api 🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕'
+
+
+
+#con el metodo run levantamos el servidor
+app.run(debug=True)
