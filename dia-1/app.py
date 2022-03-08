cb519c6e412624d53d036a135fa3d97d8ed1496d
ozmarshall <parc.tec20@yahoo.com>
2022-03-07 23:02:50 -0500

    entorno

--- dia-1/app.py
+++ dia-1/app.py
@@ -1,5 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+from datetime import datetime
 
 app = Flask(__name__)
+
+clientes = []
+
+@app.route('/')
+def estado():
+    hora_del_servidor = datetime.now()
+    return {
+        'status': True,
+        'hour': hora_del_servidor.strftime('%d/%m/Y %H:%M:%S') 
+    }
+
+@app.route('/clientes', methods= ['POST'])
+def obtener_clientes():
+
+    print(request.method)
+    print(request.data)
+    print(request.get_json())
+
+    data= request.get_json()
+    clientes.append(data)
+
+    return {
+        'message': 'cliente agragado exitosamnete',
+        'cliente': data
+    }
+
 
 app.run(debug=True)
