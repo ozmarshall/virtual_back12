f4527dffaeb98786476e7fef9228f23f620b2524
ozmarshall <parc.tec20@yahoo.com>
2022-03-24 20:00:15 -0500

    004

--- dtos/paginacion_dto.py
+++
@@ -1,6 +0,0 @@
-from config import validador
-from marshmallow import fields
-
-class PaginacionRequestDTO(validador.Schema):
-    page = fields.Integer(required=False, load_default=1) 
-    perPage = fields.Integer(required=False, load_default=10)
