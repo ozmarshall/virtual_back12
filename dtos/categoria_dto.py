61b70f53cf3549d37a76430acd016f094a03c575
ozmarshall <parc.tec20@yahoo.com>
2022-04-04 19:50:51 -0500

    prueba

--- dtos/categoria_dto.py
+++
@@ -1,7 +0,0 @@
-from pyexpat import model
-from config import validador
-from models.categorias import Categoria
-
-class CategoriaResponseDTO(validador.SQLAlchemyAutoSchema):
-    class Meta:
-       model = Categoria
