bb6d4f90114eff1e36ee754ad8cff76bbf64bc15
ozmarshall <parc.tec20@yahoo.com>
2022-04-11 19:58:47 -0500

    lo

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gestion',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/archivos/'
+# sirve para ubicar de donde se extraeran los archivos solicitados por la url y ademas donde se almaceran los archivo mediante el metodo default_storage
+
+MEDIA_ROOT = BASE_DIR / 'imagenes'
--- agenda/gestion/tests.py
+++ agenda/gestion/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class EtiquetasTestCase(APITestCase):
+    def test_crear_etiqueta_success(self):
+        request=self.client.post('/gestion/etiquetas',data={
+            'nombre': 'Frontend'
+        })
+
+        print(request.data)
+        self.assertEqual(request.status_code, 201)
+
+
+        self.assertEqual(1,1)
+
+    def test_listar_etiquetas_failed(self):
+        request=self.client.get('/gestion/etiquetas')
+        print(request.data)
+        self.assertEqual(1,1)
