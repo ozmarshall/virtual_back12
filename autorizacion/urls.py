e846be4d604aa49c18e737543a671af6be35deef
ozmarshall <parc.tec20@yahoo.com>
2022-04-25 22:40:43 -0500

    semana09

--- autorizacion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegistroUsuarioApiView
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-urlpatterns = [
-    path('registro/', RegistroUsuarioApiView.as_view()),
-    path('inicio-sesion/', TokenObtainPairView.as_view())
-]
